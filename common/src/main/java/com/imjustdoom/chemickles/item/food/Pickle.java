eb8b49fba4eb4e213b525ddb8d1d656449431287
JustDoom <61824552+JustDoom@users.noreply.github.com>
2023-06-04 17:58:01 +1000

    Get a proper effect system done

--- common/src/main/java/com/imjustdoom/chemickles/item/food/Pickle.java
+++ common/src/main/java/com/imjustdoom/chemickles/item/food/Pickle.java
@@ -1,26 +1,31 @@
 package com.imjustdoom.chemickles.item.food;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.UnmodifiableIterator;
 import com.imjustdoom.chemickles.Chemickles;
 import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.core.NonNullList;
+import net.minecraft.core.Registry;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Iterator;
+import java.util.List;
 
 public class Pickle extends Item {
-
-    //private final ImmutableList<MobEffectInstance> effects;
 
     public Pickle() {
         super(new Item.Properties()
@@ -33,21 +38,20 @@ public class Pickle extends Item {
     }
 
     @Override
-    public ItemStack finishUsingItem(ItemStack item, Level level, LivingEntity entity) {
-        Player player = entity instanceof Player ? (Player) entity : null;
+    public ItemStack finishUsingItem(ItemStack itemStack, Level level, LivingEntity livingEntity) {
+        Player player = livingEntity instanceof Player ? (Player)livingEntity : null;
         if (player instanceof ServerPlayer) {
-            CriteriaTriggers.CONSUME_ITEM.trigger((ServerPlayer) player, item);
+            CriteriaTriggers.CONSUME_ITEM.trigger((ServerPlayer)player, itemStack);
         }
 
         if (!level.isClientSide) {
-            Iterator<MobEffectInstance> var5 = PotionUtils.getMobEffects(item).iterator();
+            List<MobEffectInstance> list = PotionUtils.getMobEffects(itemStack);
 
-            while (var5.hasNext()) {
-                MobEffectInstance mobeffectinstance = var5.next();
-                if (mobeffectinstance.getEffect().isInstantenous()) {
-                    mobeffectinstance.getEffect().applyInstantenousEffect(player, player, entity, mobeffectinstance.getAmplifier(), 1.0);
+            for (MobEffectInstance mobEffectInstance : list) {
+                if (mobEffectInstance.getEffect().isInstantenous()) {
+                    mobEffectInstance.getEffect().applyInstantenousEffect(player, player, livingEntity, mobEffectInstance.getAmplifier() / 2, 1.0);
                 } else {
-                    entity.addEffect(new MobEffectInstance(mobeffectinstance));
+                    livingEntity.addEffect(new MobEffectInstance(mobEffectInstance));
                 }
             }
         }
@@ -55,21 +59,31 @@ public class Pickle extends Item {
         if (player != null) {
             player.awardStat(Stats.ITEM_USED.get(this));
             if (!player.getAbilities().instabuild) {
-                item.shrink(1);
+                itemStack.shrink(1);
             }
         }
 
-        if (player == null || !player.getAbilities().instabuild) {
-            if (item.isEmpty()) {
-                return new ItemStack(Items.GLASS_BOTTLE);
-            }
+        livingEntity.gameEvent(GameEvent.EAT);
+        return itemStack;
+    }
 
-            if (player != null) {
-                player.getInventory().add(new ItemStack(Items.GLASS_BOTTLE));
+    @Override
+    public void fillItemCategory(CreativeModeTab creativeModeTab, NonNullList<ItemStack> nonNullList) {
+        if (this.allowedIn(creativeModeTab)) {
+
+            for (Potion potion : Registry.POTION) {
+                if (potion != Potions.EMPTY) {
+                    nonNullList.add(PotionUtils.setPotion(new ItemStack(this), potion));
+                }
             }
         }
+    }
 
-        entity.gameEvent(GameEvent.DRINK);
-        return item;
+    public String getDescriptionId(ItemStack itemStack) {
+        return PotionUtils.getPotion(itemStack).getName(this.getDescriptionId() + ".effect.");
+    }
+
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> list, TooltipFlag tooltipFlag) {
+        PotionUtils.addPotionTooltip(itemStack, list, 1.0F);
     }
 }
