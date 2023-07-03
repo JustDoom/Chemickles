ea02fc71bcce22f4af7ad51ea973c6a8f81849e2
JustDoom <61824552+JustDoom@users.noreply.github.com>
2023-07-03 10:41:11 +1000

    try stuff

--- common/src/main/java/com/imjustdoom/chemickles/item/food/Pickle.java
+++ common/src/main/java/com/imjustdoom/chemickles/item/food/Pickle.java
@@ -1,7 +1,5 @@
 package com.imjustdoom.chemickles.item.food;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.UnmodifiableIterator;
 import com.imjustdoom.chemickles.Chemickles;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.NonNullList;
@@ -13,16 +11,17 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Iterator;
 import java.util.List;
 
 public class Pickle extends Item {
@@ -49,9 +48,9 @@ public class Pickle extends Item {
 
             for (MobEffectInstance mobEffectInstance : list) {
                 if (mobEffectInstance.getEffect().isInstantenous()) {
-                    mobEffectInstance.getEffect().applyInstantenousEffect(player, player, livingEntity, mobEffectInstance.getAmplifier() / 2, 1.0);
+                    mobEffectInstance.getEffect().applyInstantenousEffect(player, player, livingEntity, mobEffectInstance.getAmplifier(), 1);
                 } else {
-                    livingEntity.addEffect(new MobEffectInstance(mobEffectInstance));
+                    livingEntity.addEffect(new MobEffectInstance(mobEffectInstance.getEffect(), mobEffectInstance.getDuration() / 3, mobEffectInstance.getAmplifier(), mobEffectInstance.isAmbient(), mobEffectInstance.isVisible(), mobEffectInstance.showIcon()));
                 }
             }
         }
@@ -72,8 +71,26 @@ public class Pickle extends Item {
         if (this.allowedIn(creativeModeTab)) {
 
             for (Potion potion : Registry.POTION) {
-                if (potion != Potions.EMPTY) {
-                    nonNullList.add(PotionUtils.setPotion(new ItemStack(this), potion));
+                if (potion != Potions.EMPTY && (potion != Potions.TURTLE_MASTER && potion != Potions.LONG_TURTLE_MASTER && potion != Potions.STRONG_TURTLE_MASTER && potion != Potions.LUCK)) {
+                    //nonNullList.add(PotionUtils.setPotion(new ItemStack(this), potion));
+
+                    ItemStack potionItemStack = new ItemStack(this);
+                    PotionUtils.setPotion(potionItemStack, potion);
+
+                    // Get the PotionEffect associated with the potion
+                    List<MobEffectInstance> effects = PotionUtils.getMobEffects(potionItemStack);
+                    if (!effects.isEmpty()) {
+                        MobEffectInstance effect = effects.get(0); // Assuming there's only one effect per potion
+                        int newDuration = 200; // Modify the duration to your desired value
+
+                        // Create a new custom PotionEffect with the modified duration
+                        MobEffectInstance customEffect = new MobEffectInstance(effect.getEffect(), newDuration, effect.getAmplifier(), effect.isAmbient(), effect.isVisible(), effect.showIcon());
+
+                        // Replace the existing PotionEffect with the custom one
+                        effects.set(0, customEffect);
+                    }
+
+                    nonNullList.add(potionItemStack);
                 }
             }
         }
