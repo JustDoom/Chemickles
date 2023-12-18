acffe0f088665d31d57301882768fb646f1de0d5
JustDoom <61824552+JustDoom@users.noreply.github.com>
2023-12-18 11:58:54 +1000

    Fix the rest of the broken code

--- common/src/main/java/com/imjustdoom/chemickles/Chemickles.java
+++ common/src/main/java/com/imjustdoom/chemickles/Chemickles.java
@@ -2,6 +2,7 @@ package com.imjustdoom.chemickles;
 
 import com.imjustdoom.chemickles.block.BlockInit;
 import com.imjustdoom.chemickles.item.ItemInit;
+import com.imjustdoom.chemickles.item.Pickles;
 import dev.architectury.injectables.annotations.ExpectPlatform;
 import dev.architectury.registry.CreativeTabRegistry;
 import dev.architectury.registry.fuel.FuelRegistry;
@@ -9,17 +10,54 @@ import dev.architectury.registry.registries.RegistrySupplier;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.level.block.Block;
+
+import java.util.List;
 
 public class Chemickles {
     public static final String MOD_ID = "chemickles";
 
-    public static final CreativeTabRegistry.TabSupplier CHEMICKLES_TAB = CreativeTabRegistry.create(new ResourceLocation(MOD_ID, "chemickles_tab"), () -> new ItemStack(ItemInit.PICKLE.get()));
+    public static final CreativeTabRegistry.TabSupplier CHEMICKLES_TAB = CreativeTabRegistry.create(
+            new ResourceLocation(MOD_ID, "chemickles_tab"),
+            builder -> {
+                builder.icon(() -> new ItemStack(ItemInit.PICKLE.get()));
+                builder.displayItems((params, output) -> {
+                    for (Potion potion : BuiltInRegistries.POTION) {
+                        if (potion != Potions.EMPTY && (potion != Potions.TURTLE_MASTER && potion != Potions.LONG_TURTLE_MASTER && potion != Potions.STRONG_TURTLE_MASTER && potion != Potions.LUCK)) {
+                            //nonNullList.add(PotionUtils.setPotion(new ItemStack(this), potion));
+
+                            ItemStack potionItemStack = new ItemStack(ItemInit.PICKLE.get());
+                            PotionUtils.setPotion(potionItemStack, potion);
+
+                            // Get the PotionEffect associated with the potion
+                            List<MobEffectInstance> effects = PotionUtils.getMobEffects(potionItemStack);
+                            if (!effects.isEmpty()) {
+                                MobEffectInstance effect = effects.get(0); // Assuming there's only one effect per potion
+                                int newDuration = 200; // Modify the duration to your desired value
+
+                                // Create a new custom PotionEffect with the modified duration
+                                MobEffectInstance customEffect = new MobEffectInstance(effect.getEffect(), newDuration, effect.getAmplifier(), effect.isAmbient(), effect.isVisible(), effect.showIcon());
+
+                                // Replace the existing PotionEffect with the custom one
+                                effects.set(0, customEffect);
+                            }
+
+                            output.accept(potionItemStack);
+                        }
+                    }
+                });
+            });
 
     public static void init() {
         BlockInit.init();
--- common/src/main/java/com/imjustdoom/chemickles/item/food/Pickle.java
+++ common/src/main/java/com/imjustdoom/chemickles/item/food/Pickle.java
@@ -2,9 +2,6 @@ package com.imjustdoom.chemickles.item.food;
 
 import com.imjustdoom.chemickles.Chemickles;
 import net.minecraft.advancements.CriteriaTriggers;
-import net.minecraft.core.NonNullList;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.stats.Stats;
@@ -12,15 +9,14 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.*;
-import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.alchemy.PotionUtils;
-import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Iterator;
 import java.util.List;
 
 public class Pickle extends Item {
@@ -64,66 +60,6 @@ public class Pickle extends Item {
         livingEntity.gameEvent(GameEvent.DRINK);
         return itemStack;
     }
-
-//    @Override
-//    public ItemStack finishUsingItem(ItemStack itemStack, Level level, LivingEntity livingEntity) {
-//        Player player = livingEntity instanceof Player ? (Player) livingEntity : null;
-//        if (player instanceof ServerPlayer) {
-//            CriteriaTriggers.CONSUME_ITEM.trigger((ServerPlayer) player, itemStack);
-//        }
-//
-//        if (!level.isClientSide) {
-//            List<MobEffectInstance> list = PotionUtils.getMobEffects(itemStack);
-//
-//            for (MobEffectInstance mobEffectInstance : list) {
-//                if (mobEffectInstance.getEffect().isInstantenous()) {
-//                    mobEffectInstance.getEffect().applyInstantenousEffect(player, player, livingEntity, mobEffectInstance.getAmplifier(), 1);
-//                } else {
-//                    livingEntity.addEffect(new MobEffectInstance(mobEffectInstance.getEffect(), mobEffectInstance.getDuration(), mobEffectInstance.getAmplifier(), mobEffectInstance.isAmbient(), mobEffectInstance.isVisible(), mobEffectInstance.showIcon()));
-//                }
-//            }
-//        }
-//
-//        if (player != null) {
-//            player.awardStat(Stats.ITEM_USED.get(this));
-//            if (!player.getAbilities().instabuild) {
-//                itemStack.shrink(1);
-//            }
-//        }
-//
-//        livingEntity.gameEvent(GameEvent.EAT);
-//        return itemStack;
-//    }
-
-//    @Override
-//    public void fillItemCategory(CreativeModeTab creativeModeTab, NonNullList<ItemStack> nonNullList) {
-//        if (this.allowedIn(creativeModeTab)) {
-//
-//            for (Potion potion : Registries.POTION) {
-//                if (potion != Potions.EMPTY && (potion != Potions.TURTLE_MASTER && potion != Potions.LONG_TURTLE_MASTER && potion != Potions.STRONG_TURTLE_MASTER && potion != Potions.LUCK)) {
-//                    //nonNullList.add(PotionUtils.setPotion(new ItemStack(this), potion));
-//
-//                    ItemStack potionItemStack = new ItemStack(this);
-//                    PotionUtils.setPotion(potionItemStack, potion);
-//
-//                    // Get the PotionEffect associated with the potion
-//                    List<MobEffectInstance> effects = PotionUtils.getMobEffects(potionItemStack);
-//                    if (!effects.isEmpty()) {
-//                        MobEffectInstance effect = effects.get(0); // Assuming there's only one effect per potion
-//                        int newDuration = 200; // Modify the duration to your desired value
-//
-//                        // Create a new custom PotionEffect with the modified duration
-//                        MobEffectInstance customEffect = new MobEffectInstance(effect.getEffect(), newDuration, effect.getAmplifier(), effect.isAmbient(), effect.isVisible(), effect.showIcon());
-//
-//                        // Replace the existing PotionEffect with the custom one
-//                        effects.set(0, customEffect);
-//                    }
-//
-//                    nonNullList.add(potionItemStack);
-//                }
-//            }
-//        }
-//    }
 
     public String getDescriptionId(ItemStack itemStack) {
         return PotionUtils.getPotion(itemStack).getName(this.getDescriptionId() + ".effect.");
